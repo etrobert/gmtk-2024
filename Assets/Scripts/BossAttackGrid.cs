4de2b6c589a019e0adb00e49f12f6590f925971b
Palomia <amaury.palomino@outlook.com>
2024-08-18 19:12:02 +0200

    Fix spawn zone attack

--- Assets/Scripts/BossAttackGrid.cs
+++ Assets/Scripts/BossAttackGrid.cs
@@ -13,19 +13,28 @@ public class BossAttackGrid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        createAttack();
+        lastSpawnTime = Time.time;
     }
 
-    private int nbCircle = 200;
+    // Interval spawn time
+    public float intervalSpawnTime = 0.25f;
+    // Last Spawn Time
+    private float lastSpawnTime = 0f;
 
-    void createAttack()
+    void FixedUpdate()
     {
-        for (int i = 0; i < nbCircle; ++i)
+        //If the shape has load, shoot
+        if (Time.time - lastSpawnTime >= intervalSpawnTime)
         {
-            Vector2 randomPoint = Random.insideUnitCircle * floor.transform.localScale.x / 2f;
-            Vector3 newPosition = new(randomPoint[0], floor.transform.localScale.y + cylinderAttack.transform.localScale.y, randomPoint[1]);
-
-            Instantiate(cylinderAttack, newPosition, cylinderAttack.transform.rotation);
+            SpawnCylinder();
+            lastSpawnTime = Time.time;
         }
+    }
+
+    void SpawnCylinder()
+    {
+        Vector2 randomPoint = Random.insideUnitCircle * floor.transform.localScale.x / 2f;
+        Vector3 newPosition = new(randomPoint[0], floor.transform.localScale.y + cylinderAttack.transform.localScale.y, randomPoint[1]);
+        Instantiate(cylinderAttack, newPosition, cylinderAttack.transform.rotation);
     }
 }
