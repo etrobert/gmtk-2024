2e742291365ab7572741444956d055be11e0e7b9
Palomia <amaury.palomino@outlook.com>
2024-08-19 00:58:58 +0200

    Add Boss Health Bar

---
+++ Assets/Scripts/BossHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+    [SerializeField] public float health, maxHealth = 1000;
+
+    [SerializeField] BossHealthBar healthBar;
+
+    private void Start()
+    {
+        health = maxHealth;
+        healthBar.UpdateHealthBar(health, maxHealth);
+    }
+
+    private void Die()
+    {
+        Debug.Log("YOU WIN!");
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        healthBar.UpdateHealthBar(health, maxHealth);
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+}
