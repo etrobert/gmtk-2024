5f489364e3fcbfb35b530fc111faceb3c7c44b1a
limichou <paulab.yon@gmail.com>
2024-08-19 13:33:29 +0200

    Add helice and camera view

--- Assets/Scripts/Boss/BossRotation.cs
+++ Assets/Scripts/Boss/BossRotation.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BossRotation : MonoBehaviour
 
 {
+    public int rotationDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,6 @@ public class BossRotation : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Rotate(0, 0.1f, 0);
+        transform.Rotate(0, rotationDirection * 0.1f, 0);
     }
 }
