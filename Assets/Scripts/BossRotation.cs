5501c78041782da21457682d5a1a1db0e751a807
limichou <paulab.yon@gmail.com>
2024-08-19 04:00:33 +0200

    Update the shell

--- Assets/Scripts/BossRotation.cs
+++ Assets/Scripts/BossRotation.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BossRotation : MonoBehaviour
+
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Rotate(0,0.1f,0);   
+        transform.Rotate(0, 0.1f, 0);
     }
 }
