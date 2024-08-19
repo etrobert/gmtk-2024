b5f373dff26fe8377a8d550b165602c5b8b58ad2
limichou <paulab.yon@gmail.com>
2024-08-19 02:45:02 +0200

    Destroy laser

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,11 +3,23 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float bulletSpeed = 20f;
+    private float initialSpawnTime = 0f;
+    private readonly float timeToDestroy = 5f;
+
+    void Start()
+    {
+        initialSpawnTime = Time.time;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.position += transform.forward * bulletSpeed * Time.deltaTime;
-
+        
+        //If it's time to destroy the bullet
+        if (Time.time - initialSpawnTime >= timeToDestroy)
+        {
+            Destroy(gameObject);
+        }
     }
 }
