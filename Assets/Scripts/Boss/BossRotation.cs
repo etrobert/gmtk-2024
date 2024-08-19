83b9e6ecb5f99dff5c79f0dbb8584d2bcc2d3847
limichou <paulab.yon@gmail.com>
2024-08-19 14:13:30 +0200

    Modify colors and speed of helice

--- Assets/Scripts/Boss/BossRotation.cs
+++ Assets/Scripts/Boss/BossRotation.cs
@@ -6,7 +6,7 @@ using UnityEngine.UIElements;
 public class BossRotation : MonoBehaviour
 
 {
-    public int rotationDirection;
+    public float rotationDirection;
     // Start is called before the first frame update
     void Start()
     {
