562edb611fe4d595491a666d30d5f5beb6862881
Etienne Robert <etiennerobert33@gmail.com>
2024-08-20 16:45:45 +0200

    add shell on levelup

--- Assets/Scripts/Boss/LevelShell.cs
+++ Assets/Scripts/Boss/LevelShell.cs
@@ -9,12 +9,17 @@ public class LevelShell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        transform.position = new Vector3(transform.position.x, -10f, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
+        int BossLevel = boss.GetComponent<BossLevel>().GetLevel();
+        if (BossLevel >= 2)
+        {
+            transform.position = new Vector3(transform.position.x, 4.74f, transform.position.z);
+        }
 
     }
 }
