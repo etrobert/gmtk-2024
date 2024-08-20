ee85366c980005ac50697ce35e8452c88dd793f6
Etienne Robert <etiennerobert33@gmail.com>
2024-08-20 16:37:42 +0200

    link levels to boss

--- Assets/Scripts/Boss/Level1Helices.cs
+++ Assets/Scripts/Boss/Level1Helices.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Level1Helices : MonoBehaviour
 {
+    public GameObject boss;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/Boss/LevelShell.cs
+++ Assets/Scripts/Boss/LevelShell.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class LevelShell : MonoBehaviour
 {
+    public GameObject boss;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
