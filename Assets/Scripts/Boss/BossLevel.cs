c50eaebe3f84f496e889165f1b89a3c853e5142d
Etienne Robert <etiennerobert33@gmail.com>
2024-08-20 15:40:43 +0200

    add boss level component

--- Assets/Scripts/Boss/BossLevel.cs
+++ Assets/Scripts/Boss/BossLevel.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BossLevel : MonoBehaviour
 {
+    public int level = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        level = 1;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void IncreaseLevel()
     {
-        
+        level++;
     }
 }
