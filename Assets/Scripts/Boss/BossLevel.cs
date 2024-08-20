d219688b48adf9b4ea379d4ae5d030657569be48
Etienne Robert <etiennerobert33@gmail.com>
2024-08-20 17:18:04 +0200

    UI boss level

--- Assets/Scripts/Boss/BossLevel.cs
+++ Assets/Scripts/Boss/BossLevel.cs
@@ -1,16 +1,22 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossLevel : MonoBehaviour
 {
     public int level = 1;
+    public TMP_Text text;
+
     // Start is called before the first frame update
     void Start()
     {
         level = 1;
+        text.text = "lvl: " + level;
     }
     public void IncreaseLevel()
     {
         level++;
+        text.text = "lvl: " + level;
         if (level > 6)
         {
             MainMenu.QuitGame();
